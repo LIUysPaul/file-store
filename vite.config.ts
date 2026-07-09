f8a2a99e07e7dbca436b39a5abf26ce973a3bc17
LIUysPaul <bestpigever@163.com>
2026-07-09 04:40:58 +0000

    build: 更新配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
-import { traeBadgePlugin } from 'vite-plugin-trae-solo-badge';
 
 export default defineConfig({
-  base: '/',
+  base: '/file-store/',
   build: {
     sourcemap: 'hidden',
     outDir: 'dist',
   },
   plugins: [
-    react({
-      babel: {
-        plugins: [
-          'react-dev-locator',
-        ],
-      },
-    }),
-    traeBadgePlugin({
-      variant: 'dark',
-      position: 'bottom-right',
-      prodOnly: true,
-      clickable: true,
-      clickUrl: 'https://www.trae.ai/solo?showJoin=1',
-      autoTheme: true,
-      autoThemeTarget: '#root'
-    }), 
+    react(),
     tsconfigPaths()
   ],
 })
