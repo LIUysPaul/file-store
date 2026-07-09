b7fc1a0f0483c0b43944b8d5d86354a7491ac7bf
LIUysPaul <bestpigever@163.com>
2026-07-09 08:10:33 +0000

    fix: 恢复 HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { Home } from "@/pages/Home";
 import { Login } from "@/pages/Login";
 import { useAuthStore } from "@/stores/authStore";
